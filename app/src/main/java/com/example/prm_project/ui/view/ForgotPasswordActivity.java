a787897d1dfb9853be12b6adcef642fb5066486a
canxi4589 <caotri1203@gmail.com>
2025-07-10 13:40:14 +0700

    minor add

---
+++ app/src/main/java/com/example/prm_project/ui/view/ForgotPasswordActivity.java
@@ -0,0 +1,36 @@
+package com.example.prm_project.ui.view;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import com.example.prm_project.R;
+import com.example.prm_project.databinding.ActivityForgotPasswordBinding;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+    
+    private ActivityForgotPasswordBinding binding;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Set up data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_forgot_password);
+        
+        // Initialize UI components
+        initializeComponents();
+    }
+    
+    private void initializeComponents() {
+        // TODO: Initialize UI components and set up listeners
+        // This will be implemented when adding logic
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (binding != null) {
+            binding.unbind();
+        }
+    }
+} 
---
+++ app/src/main/java/com/example/prm_project/ui/view/ResetEmailSentActivity.java
@@ -0,0 +1,36 @@
+package com.example.prm_project.ui.view;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import com.example.prm_project.R;
+import com.example.prm_project.databinding.ActivityResetEmailSentBinding;
+
+public class ResetEmailSentActivity extends AppCompatActivity {
+    
+    private ActivityResetEmailSentBinding binding;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Set up data binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reset_email_sent);
+        
+        // Initialize UI components
+        initializeComponents();
+    }
+    
+    private void initializeComponents() {
+        // TODO: Initialize UI components and set up listeners
+        // This will be implemented when adding logic
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (binding != null) {
+            binding.unbind();
+        }
+    }
+} 
